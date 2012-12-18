0eb5f2a500aa46e2d707e69d7ca69c9f40e43686
Silviu Durduc <iamsilviu@gmail.com>
2012-12-18 17:58:44 +0200

    Doodle view create

---
+++ source/app/view/Doodle.js
@@ -0,0 +1,28 @@
+/* 
+ * Doodle Canvas
+ */
+
+Ext.define('DoodleCalc.view.Doodle',{
+    extend: 'Ext.Panel',
+    alias: 'widget.Doodle',
+    
+    config : {
+       
+        layout: 'fit',
+        items: [
+            {
+                xtype: 'draw',
+
+                items: [
+                    {
+                        type: 'path',
+                        path: 'M75,75 c0,-25 50,25 50,0 c0,-25 -50,25 -50,0',
+                        strokeStyle: 'blue'
+                    }       
+                ]
+
+
+            }
+        ]
+    }
+});
